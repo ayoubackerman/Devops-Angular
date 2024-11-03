3b296bdf6b0d688c17c7db6fd256a1b381f097fc
Ayoub_Ben_Brahim <96891804+ayoubackerman@users.noreply.github.com>
2024-11-03 07:36:16 +0200

    Update departement.service.ts

--- src/app/Services/Departement/departement.service.ts
+++ src/app/Services/Departement/departement.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const BASE_URL ="http://localhost:8089/kaddem/";
+const BASE_URL ="http://Kaddem:8089/kaddem/";
 
 
 @Injectable({
